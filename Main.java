01f6c31f49606ba181a80e7cb8fdd68b1953c9c6
aryana04 <aryanakirksey@gmail.com>
2025-04-02 14:01:59 -0400

    Updated Main.java
    Updated Main.java

--- Main.java
+++ Main.java
@@ -13,7 +13,8 @@ public class Main extends World
      * 
      */
     public Main()
-    {    
+    {
+        
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         GreenfootImage bg = new GreenfootImage("Title.png");
